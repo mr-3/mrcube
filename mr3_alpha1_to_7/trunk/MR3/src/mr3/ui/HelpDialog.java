51fec80a370c06330ab8ca9b09640f561f3705f0
Takeshi Morita <takesi.morita@gmail.com>
2003-07-17 05:10:42 +0000

    - import,exportのバグ修正． - Helpダイアログ修正．
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@98 2d18f6ef-ed67-4be4-865d-64751944eb35

--- mr3_alpha1_to_7/trunk/MR3/src/mr3/ui/HelpDialog.java
+++ mr3_alpha1_to_7/trunk/MR3/src/mr3/ui/HelpDialog.java
@@ -5,6 +5,7 @@
 package mr3.ui;
 
 import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 
@@ -13,30 +14,44 @@ import javax.swing.*;
  */
 public class HelpDialog extends JDialog {
 
-	private static final String TOOL_NAME = "<h2>MR<sup>3</sup> <br> (Meta-Model Management based on <br> RDFs Revision Reflection)</h2>";
+	private static final String TOOL_NAME = "<h2>MR<sup>3</sup></h2> (Meta-Model Management based on <br> RDFs Revision Reflection)";
+	private static final String MR3_URL = "http://panda.cs.inf.shizuoka.ac.jp/mmm/mr3#";
 	private static final String VERSION = "ƒ¿6 (2003-07-)";
-
+	private static final Color DESKTOP_BACK_COLOR = new Color(225, 225, 225);
+	
 	public HelpDialog(ImageIcon logo) {
-		super((Frame) null, "About MR^3", true);
+		super((Frame) null, "About MR^3", true);	
+		JLabel logoLabel = new JLabel("<html>"+TOOL_NAME+"</html>",logo, SwingConstants.LEFT);
+		logoLabel.setFont(logoLabel.getFont().deriveFont(Font.PLAIN, 16));
+		JLabel brLabel = new JLabel("<html><br></html>");
+		JLabel urlLabel = new JLabel("   URL: " + MR3_URL);
+		urlLabel.setFont(urlLabel.getFont().deriveFont(Font.PLAIN, 14));
+		JLabel versionLabel = new JLabel("   Version: " + VERSION);
+		versionLabel.setFont(versionLabel.getFont().deriveFont(Font.PLAIN, 14));
+		
 		JPanel messagePanel = new JPanel();
-		JLabel logoLabel = new JLabel(logo);
-		logoLabel.setFont(logoLabel.getFont().deriveFont(Font.PLAIN, 24));
-		JLabel toolNameLabel = new JLabel("<html>" + TOOL_NAME + "</html>");
-		toolNameLabel.setFont(toolNameLabel.getFont().deriveFont(Font.PLAIN, 24));
-		JLabel versionLabel = new JLabel("Version " + VERSION);
-		versionLabel.setFont(versionLabel.getFont().deriveFont(Font.PLAIN, 16));
-
 		messagePanel.setLayout(new BoxLayout(messagePanel, BoxLayout.Y_AXIS));
-		messagePanel.add(toolNameLabel);
+		messagePanel.add(brLabel);
+		messagePanel.add(urlLabel);
 		messagePanel.add(versionLabel);
 
+		JButton confirmButton = new JButton("OK");
+		confirmButton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+
 		Container contentPane = getContentPane();
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
-		contentPane.add(logoLabel);
-		contentPane.add(messagePanel);
-		setBounds(300, 300, 450, 200);
+		contentPane.setLayout(new BorderLayout());
+		contentPane.add(logoLabel, BorderLayout.NORTH);
+		contentPane.add(messagePanel, BorderLayout.CENTER);
+		contentPane.add(confirmButton, BorderLayout.SOUTH);		
+		contentPane.setBackground(DESKTOP_BACK_COLOR);
+		messagePanel.setBackground(DESKTOP_BACK_COLOR);
+
+		setBounds(300, 300, 400, 250);
 		setResizable(false);
 		setVisible(true);
 	}
-
 }
