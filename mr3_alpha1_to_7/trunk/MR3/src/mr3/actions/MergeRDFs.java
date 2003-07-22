6f62f450d528f92ef89ac204df85c9f887c5cb8f
Takeshi Morita <takesi.morita@gmail.com>
2003-07-22 08:31:14 +0000

    - URIの入力ダイアログに例を表示し，URI入力フィールドの幅を長くした．
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@116 2d18f6ef-ed67-4be4-865d-64751944eb35

--- mr3_alpha1_to_7/trunk/MR3/src/mr3/actions/MergeRDFs.java
+++ mr3_alpha1_to_7/trunk/MR3/src/mr3/actions/MergeRDFs.java
@@ -35,7 +35,7 @@ public class MergeRDFs extends AbstractActionFile {
 		if (e.getActionCommand().equals(MERGE_RDFS_FILE)) {
 			model = readModel(getReader("rdfs", null), gmanager.getBaseURI());
 		} else if (e.getActionCommand().equals(MERGE_RDFS_URI)) {
-			String uri = JOptionPane.showInternalInputDialog(desktop, "Open URI");
+			String uri = JOptionPane.showInternalInputDialog(desktop, "Open URI ( exp. http://www.w3.org/TR/rdf-schema/rdfs-namespace.xml )");
 			model = readModel(getReader(uri), gmanager.getBaseURI());
 		}
 		mr3.mergeRDFSModel(model);		
