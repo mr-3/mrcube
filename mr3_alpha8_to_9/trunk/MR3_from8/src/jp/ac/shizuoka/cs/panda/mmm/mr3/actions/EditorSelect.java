7be4e783cd8e72b47d4939551e70906a5fa3e0f5
Takeshi Morita <takesi.morita@gmail.com>
2003-08-20 06:53:00 +0000

    - Repository変更． - Jena2に移行． - RDFリテラルのDatatypeを指定できるようになった．
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@149 2d18f6ef-ed67-4be4-865d-64751944eb35

---
+++ mr3_alpha8_to_9/trunk/MR3_from8/src/jp/ac/shizuoka/cs/panda/mmm/mr3/actions/EditorSelect.java
@@ -0,0 +1,53 @@
+/*
+ * Created on 2003/07/20
+ *
+ */
+package jp.ac.shizuoka.cs.panda.mmm.mr3.actions;
+
+import java.awt.event.*;
+import java.beans.*;
+
+import javax.swing.*;
+
+import jp.ac.shizuoka.cs.panda.mmm.mr3.*;
+
+/**
+ * @author takeshi morita
+ *
+ */
+public class EditorSelect extends MR3AbstractAction {
+
+	public EditorSelect(MR3 mr3, String name) {
+		super(mr3, name);
+	}
+
+	public EditorSelect(MR3 mr3, String name, ImageIcon icon) {
+		super(mr3, name, icon);
+	}
+
+	private static final String TO_FRONT_RDF_EDITOR = "To Front RDF Editor";
+	private static final String TO_FRONT_CLASS_EDITOR = "To Front Class Editor";
+	private static final String TO_FRONT_PROPERTY_EDITOR = "To Front Property Editor";
+
+	private void toFrontInternalFrame(JInternalFrame iFrame) {
+		try {
+			iFrame.toFront();
+			iFrame.setIcon(false);
+			iFrame.setSelected(true);
+		} catch (PropertyVetoException pve) {
+			pve.printStackTrace();
+		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		JInternalFrame[] iFrame = mr3.getInternalFrames();
+		if (getName().equals(TO_FRONT_RDF_EDITOR)) {
+			toFrontInternalFrame(iFrame[0]);
+		} else if (getName().equals(TO_FRONT_CLASS_EDITOR)) {
+			toFrontInternalFrame(iFrame[1]);
+		} else if (getName().equals(TO_FRONT_PROPERTY_EDITOR)) {
+			toFrontInternalFrame(iFrame[2]);
+		}
+	}
+
+}
---
+++ mr3_alpha8_to_9/trunk/MR3_from8/src/jp/ac/shizuoka/cs/panda/mmm/mr3/jgraph/RDFLiteralCell.java
@@ -0,0 +1,17 @@
+package jp.ac.shizuoka.cs.panda.mmm.mr3.jgraph;
+import com.jgraph.graph.*;
+
+/**
+ * @author takeshi morita
+ *
+ */
+public class RDFLiteralCell extends DefaultGraphCell {
+	public RDFLiteralCell() {
+		this(null);
+	}
+
+	public RDFLiteralCell(Object userObject) {
+		super(userObject);
+	}
+}
+
