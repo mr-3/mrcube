9eabebdc30b9b51a7883ecb08495e1c537383aae
Takeshi Morita <takesi.morita@gmail.com>
2003-09-02 12:50:28 +0000

    run.shを改良．Encodingに注意．（EUC-JP Unixに変更が必要）
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@161 2d18f6ef-ed67-4be4-865d-64751944eb35

--- mr3_alpha8_to_9/trunk/MR3_from8/run.sh
+++ mr3_alpha8_to_9/trunk/MR3_from8/run.sh
@@ -11,4 +11,4 @@ $MR3_HOME/lib/xmlParserAPIs.jar:\
 $MR3_HOME/lib/icu4j.jar:\
 $MR3_HOME/lib/junit.jar:\
 $MR3_HOME/lib/log4j-1.2.7.jar:\
-$MR3_HOME/lib/jakarta-oro-2.0.5.jar:\ jp.ac.shizuoka.cs.panda.mmm.mr3.MR3
+$MR3_HOME/lib/jakarta-oro-2.0.5.jar:" jp.ac.shizuoka.cs.panda.mmm.mr3.MR3
