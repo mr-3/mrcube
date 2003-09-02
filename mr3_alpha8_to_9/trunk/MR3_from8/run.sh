ef804da505124a120a6a62f4e4aaa48319ffe18c
Takeshi Morita <takesi.morita@gmail.com>
2003-09-02 12:44:52 +0000

    run.shを改良．
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@160 2d18f6ef-ed67-4be4-865d-64751944eb35

--- mr3_alpha8_to_9/trunk/MR3_from8/run.sh
+++ mr3_alpha8_to_9/trunk/MR3_from8/run.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-java -jar build/mr3.jar
+
+JAVA_HOME=/usr/java/j2sdk1.4.2
+MR3_HOME=.
+
+$JAVA_HOME/bin/java -classpath "\
+$MR3_HOME/mr3.jar:\
+$MR3_HOME/lib/jena.jar:\
+$MR3_HOME/lib/xercesImpl.jar:\
+$MR3_HOME/lib/xmlParserAPIs.jar:\
+$MR3_HOME/lib/icu4j.jar:\
+$MR3_HOME/lib/junit.jar:\
+$MR3_HOME/lib/log4j-1.2.7.jar:\
+$MR3_HOME/lib/jakarta-oro-2.0.5.jar:\ jp.ac.shizuoka.cs.panda.mmm.mr3.MR3
