02af2f0ef9dbedca83262e56c0bd0986d81142cc
t_morita <t_morita@2d18f6ef-ed67-4be4-865d-64751944eb35>
2003-09-25 10:53:32 +0000

    JavaDocの更新．
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@180 2d18f6ef-ed67-4be4-865d-64751944eb35

--- mr3_alpha8_to_9/trunk/MR3_from8/src/jp/ac/shizuoka/cs/panda/mmm/mr3/plugin/MR3Plugin.java
+++ mr3_alpha8_to_9/trunk/MR3_from8/src/jp/ac/shizuoka/cs/panda/mmm/mr3/plugin/MR3Plugin.java
@@ -1,7 +1,6 @@
 /*
- * Created on 2003/06/17
+ * MR^3のプラグインを作成するためのクラス
  *
- * getSelectedXXXメソッドを追加した．
  */
 package jp.ac.shizuoka.cs.panda.mmm.mr3.plugin;
 
@@ -37,83 +36,155 @@ public abstract class MR3Plugin {
 		this.mr3 = mr3;
 	}
 
+	/**
+	 * MR3Pluginクラスのサブクラスで実装する．
+	 * File->pluginsに追加されるメニューを実行すると，execメソッドが実行される．
+	 */
 	public abstract void exec();
 
 	public String toString() {
 		return menuName;
 	}
 
-	protected void replaceRDFModel(Model model) {		
+	/** 
+	 *  Jenaが提供するModelを，MR3のRDFグラフへ変換する．
+	 *  変換したRDFグラフを編集中のRDFグラフと置換する．
+	 */
+	protected void replaceRDFModel(Model model) {
 		mr3.replaceRDFModel(model);
 		mr3.getGraphManager().applyTreeLayout();
 	}
 
+	/** 
+	 *  Jenaが提供するModelを，MR3のRDFグラフへ変換する．
+	 *  変換したRDFグラフを編集中のRDFグラフにマージする．
+	 */
 	protected void mergeRDFModel(Model model) {
 		mr3.mergeRDFModel(model);
 		mr3.getGraphManager().applyTreeLayout();
 	}
 
+	/** 
+	 *  Jenaが提供するModelを，MR3のRDFSグラフへ変換する．
+	 *  変換したRDFSグラフを編集中のRDFSグラフにマージする．
+	 */
 	protected void mergeRDFSModel(Model model) {
 		mr3.mergeRDFSModel(model);
 		mr3.getGraphManager().applyTreeLayout();
 	}
 
+	/**
+	 * Jenaが提供するModelを，MR^3のプロジェクトへ変換する．
+	 * @param MR^3のプロジェクトファイル． 
+	 */
 	protected void replaceProjectModel(Model model) {
 		mr3.replaceProjectModel(model);
 	}
-	
+
+	/**
+	 * MR3のRDFグラフをJenaのModelに変換する．
+	 */
 	protected Model getRDFModel() {
 		return mr3.getRDFModel();
 	}
 
+	/**
+	 * 選択されているMR3のRDFグラフをJenaのModelに変換する．
+	 */
 	protected Model getSelectedRDFModel() {
 		return mr3.getSelectedRDFModel();
 	}
 
+	/**
+	 * MR3のRDFSグラフをJenaのModelに変換する．
+	 */
 	protected Model getRDFSModel() {
 		return mr3.getRDFSModel();
 	}
 
+	/**
+	 * 選択されているMR3のRDFSグラフをJenaのModelに変換する．
+	 */
 	protected Model getSelectedRDFSModel() {
 		return mr3.getSelectedRDFSModel();
 	}
 
+	/**
+	 * MR3のクラスグラフをJenaのModelに変換する．
+	 * @return
+	 */
 	protected Model getClassModel() {
 		return mr3.getClassModel();
 	}
 
+	/**
+	 * 選択されているMR3のクラスグラフをJenaのModelに変換する．
+	 * @return
+	 */
 	protected Model getSelectedClassModel() {
 		return mr3.getSelectedClassModel();
 	}
 
+	/**
+	 * MR3のプロパティグラフをJenaのModelに変換する．
+	 * @return
+	 */
 	protected Model getPropertyModel() {
 		return mr3.getPropertyModel();
 	}
 
+	/**
+	 * 選択されているMR3のプロパティグラフをJenaのModelに変換する．
+	 * @return
+	 */
 	protected Model getSelectedPropertyModel() {
 		return mr3.getSelectedPropertyModel();
 	}
 
+	/**
+	 * プロジェクトをJenaのModelに変換する．
+	 * @return
+	 */
 	protected Model getProjectModel() {
 		return mr3.getProjectModel();
 	}
 
+	/**
+	 * RDFグラフ(org.jgraph.JGraph)を得る．
+	 * @return org.jgraph.JGraph
+	 */
 	protected JGraph getRDFGraph() {
 		return mr3.getRDFGraph();
 	}
 
+	/**
+	 * クラスグラフ(org.jgraph.JGraph)を得る．
+	 * @return org.jgraph.JGraph
+	 */
 	protected JGraph getClassGraph() {
 		return mr3.getClassGraph();
 	}
 
-	protected JGraph getPropertyGraph(){
+	/**
+	 * プロパティグラフ(org.jgraph.JGraph)を得る．
+	 * @return org.jgraph.JGraph
+	 */
+	protected JGraph getPropertyGraph() {
 		return mr3.getPropertyGraph();
 	}
 
+	/**
+	 * BaseURIを得る．
+	 * @return
+	 */
 	protected String getBaseURI() {
 		return mr3.getBaseURI();
 	}
-	
+
+	/**
+	 * JDesktopPaneを得る．内部ウィンドウを作成する際に用いる．
+	 * @return
+	 */
 	protected JDesktopPane getDesktopPane() {
 		return mr3.getDesktopPane();
 	}
