2f626a8eb67beb8d64c34c7255913923e8403a4b
t_morita <t_morita@2d18f6ef-ed67-4be4-865d-64751944eb35>
2003-10-02 06:52:27 +0000

    Localを見て切り替えるようにしていたが，設定の方に反映させていなかったのを修正．
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@201 2d18f6ef-ed67-4be4-865d-64751944eb35

--- mr3_alpha8_to_9/trunk/MR3_from8/src/jp/ac/shizuoka/cs/panda/mmm/mr3/util/Translator.java
+++ mr3_alpha8_to_9/trunk/MR3_from8/src/jp/ac/shizuoka/cs/panda/mmm/mr3/util/Translator.java
@@ -2,6 +2,9 @@ package jp.ac.shizuoka.cs.panda.mmm.mr3.util;
 
 import java.io.*;
 import java.util.*;
+import java.util.prefs.*;
+
+import jp.ac.shizuoka.cs.panda.mmm.mr3.data.*;
 
 public class Translator {
 
@@ -11,15 +14,29 @@ public class Translator {
 	public static String getString(String sKey) {
 		try {
 			return resourceBundle.getString(sKey);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return "Failed";
 		}
 	}
 
+	private static String getDefaultLocaleStr() {
+		if (Locale.getDefault().equals(Locale.JAPAN)) {
+			return "ja";
+		} else {
+			return "en";
+		}
+	}
+
+	public static void loadResourceBundle(Preferences userPrefs) {
+		String lang = (String) userPrefs.get(PrefConstants.UILang, getDefaultLocaleStr());
+		userPrefs.put(PrefConstants.UILang, (String) lang);
+		loadResourceBundle(lang);
+	}
+
 	public static void loadResourceBundle(String lang) {
-		try {			
-			InputStream ins = Utilities.class.getClassLoader().getResourceAsStream(RESOURCE_DIR + "MR3_"+lang+".properties");
+		try {
+			InputStream ins = Utilities.class.getClassLoader().getResourceAsStream(RESOURCE_DIR + "MR3_" + lang + ".properties");
 			resourceBundle = new PropertyResourceBundle(ins);
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
