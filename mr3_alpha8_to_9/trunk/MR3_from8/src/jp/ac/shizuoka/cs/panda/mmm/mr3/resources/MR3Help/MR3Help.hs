f5af6dcc960342d6ad8d2fcf466dd1867e80eed9
Takeshi Morita <takesi.morita@gmail.com>
2003-10-05 11:00:10 +0000

    JavaHelpを作成して，起動できるようにした．
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@208 2d18f6ef-ed67-4be4-865d-64751944eb35

---
+++ mr3_alpha8_to_9/trunk/MR3_from8/src/jp/ac/shizuoka/cs/panda/mmm/mr3/resources/MR3Help/MR3Help.hs
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="ISO-8859-1" ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+          "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<helpset version="2.0">
+
+   <!-- title -->
+   <title>MR^3 Help</title>
+
+   <!-- maps -->
+   <maps>
+      <homeID>intro</homeID>
+      <mapref location="Map.jhm" />
+   </maps>
+
+   <!-- views -->
+   <view>
+      <name>MR3HelpTOC</name>
+      <label>Table Of Contents</label>
+      <type>javax.help.TOCView</type>
+      <data>MR3HelpTOC.xml</data>
+      <GUI isMaximize="true" indexAll="500" index="200" />
+   </view>
+
+   <view>
+      <name>Index</name>
+      <label>Index</label>
+      <type>javax.help.IndexView</type>
+      <data>MR3HelpIndex.xml</data>
+      <GUI isMaximize="true" indexAll="500" index="200" />
+   </view>
+
+   <view>
+      <name>Search</name>
+      <label>Search</label>
+      <type>javax.help.SearchView</type>
+      <data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+      <jhindexer additionalParameter="" fileFolderArray="help" searchLogFile="" />
+      <GUI isMaximize="true" indexAll="500" index="200" />
+   </view>
+
+   <!-- presentations -->
+   <presentation xml:lang="ja" default="true">
+      <name>main window</name>
+      <size width="800" height="600" />
+      <location x="0" y="0" />
+   </presentation>
+
+</helpset>
