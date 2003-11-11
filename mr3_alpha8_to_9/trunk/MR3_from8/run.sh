012f170a0a81b416cca2a768979498de34d87dbc
t_morita <t_morita@2d18f6ef-ed67-4be4-865d-64751944eb35>
2003-11-11 08:21:07 +0000

    run.shを修正．
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@215 2d18f6ef-ed67-4be4-865d-64751944eb35

--- mr3_alpha8_to_9/trunk/MR3_from8/run.sh
+++ mr3_alpha8_to_9/trunk/MR3_from8/run.sh
@@ -2,18 +2,19 @@
 
 JAVA_HOME=/usr/java/j2sdk1.4.2
 MR3_HOME=.
+cd $MR3_HOME
 
 $JAVA_HOME/bin/java -classpath "\
-$MR3_HOME/mr3.jar:\
-$MR3_HOME/lib/antlr.debug.jar:\
-$MR3_HOME/lib/antlr.jar:\
-$MR3_HOME/lib/concurrent.jar:\
-$MR3_HOME/lib/icu4j.jar:\
-$MR3_HOME/lib/jakarta-oro-2.0.5.jar:\
-$MR3_HOME/lib/jena.jar:\
-$MR3_HOME/lib/jgraph.jar:\
-$MR3_HOME/lib/junit.jar:\
-$MR3_HOME/lib/log4j-1.2.7.jar:\
-$MR3_HOME/lib/rdf-api-2001-01-19.jar:\
-$MR3_HOME/lib/xercesImpl.jar:\
-$MR3_HOME/lib/xmlParserAPIs.jar:" org.semanticweb.mmm.mr3.MR3
+$MR3_HOME/build/mr3.jar:\
+$MR3_HOME/build/lib/antlr.debug.jar:\
+$MR3_HOME/build/lib/antlr.jar:\
+$MR3_HOME/build/lib/concurrent.jar:\
+$MR3_HOME/build/lib/icu4j.jar:\
+$MR3_HOME/build/lib/jakarta-oro-2.0.5.jar:\
+$MR3_HOME/build/lib/jena.jar:\
+$MR3_HOME/build/lib/jgraph.jar:\
+$MR3_HOME/build/lib/junit.jar:\
+$MR3_HOME/build/lib/log4j-1.2.7.jar:\
+$MR3_HOME/build/lib/rdf-api-2001-01-19.jar:\
+$MR3_HOME/build/lib/xercesImpl.jar:\
+$MR3_HOME/build/lib/xmlParserAPIs.jar:" org.semanticweb.mmm.mr3.MR3
