a8df763630cf9e213df067e642803f5584455b8a
Takeshi Morita <takesi.morita@gmail.com>
2009-10-31 17:12:51 +0000

    Tagging the Alpha2 release.
    git-svn-id: svn+ssh://svn.code.sf.net/p/mr3/code@268 2d18f6ef-ed67-4be4-865d-64751944eb35

---
+++ mr3/tags/Alpha2/src/mr3/data/RDFLiteralInfoMap.java
@@ -0,0 +1,59 @@
+package mr3.data;
+import java.io.*;
+import java.util.*;
+
+import com.hp.hpl.mesa.rdf.jena.common.*;
+import com.hp.hpl.mesa.rdf.jena.model.*;
+
+/**
+ * @author takeshi morita
+ *
+ */
+public class RDFLiteralInfoMap {
+	private Map cellInfoMap;
+	private static RDFLiteralInfoMap litInfoMap = new RDFLiteralInfoMap();
+
+	private RDFLiteralInfoMap() {
+		cellInfoMap = new HashMap();
+	}
+
+	public static RDFLiteralInfoMap getInstance() {
+		return litInfoMap;
+	}
+
+	public Literal cloneRDFLiteralInfo(Literal orgInfo) {
+		Literal newInfo = null;
+		try {
+			newInfo = new LiteralImpl(orgInfo.getString(), orgInfo.getLanguage());
+		} catch(RDFException e) {
+			e.printStackTrace();
+		}
+		return newInfo;
+	}
+
+	public void putCellInfo(Object cell, Literal info) {
+		cellInfoMap.put(cell, info);
+	}
+
+	public Literal getCellInfo(Object cell) {
+		return (Literal) cellInfoMap.get(cell);
+	}
+
+	public Serializable getState() {
+		ArrayList list = new ArrayList();
+		list.add(cellInfoMap);
+		return list;
+	}
+	
+	public void setState(Map newMap) {
+		cellInfoMap.putAll(newMap);	
+	}
+	
+	public void clear() {
+		cellInfoMap.clear();
+	}
+
+	public Collection values() {
+		return cellInfoMap.values();
+	}
+}
---
+++ mr3/tags/Alpha2/src/mr3/ui/AttributeDialog.java
@@ -0,0 +1,49 @@
+package mr3.ui;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ * @author takehshi morita
+ *
+ */
+public class AttributeDialog extends JInternalFrame implements ActionListener {
+	private static final JPanel NULL_PANEL = new JPanel();
+	private JCheckBoxMenuItem showPropDialog;
+
+	public AttributeDialog() {
+		super("Attribute Dialog", false, true);
+		showPropDialog = new JCheckBoxMenuItem("Show Attribute Dialog", true);
+		showPropDialog.addActionListener(this);
+		addInternalFrameListener(new InternalFrameAdapter() {
+			public void internalFrameClosing(InternalFrameEvent e) {
+				setVisible(false);
+			}
+		});
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		setSize(new Dimension(400, 430));
+		setLocation(50, 50);
+		setVisible(false);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		setVisible(showPropDialog.getState());
+	}
+
+	public void setNullPanel() {
+		setContentPane(NULL_PANEL);
+	}
+
+	public void setVisible(boolean b) {
+		super.setVisible(b);
+		if (showPropDialog != null) {
+			showPropDialog.setState(b);
+		}
+	}
+
+	public JCheckBoxMenuItem getShowPropWindow() {
+		return showPropDialog;
+	}
+}
