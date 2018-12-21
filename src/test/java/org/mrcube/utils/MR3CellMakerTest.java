63861fa8e2bad28a1df68d93599c642b8bf330fa
Takeshi Morita <t_morita@keio.jp>
2018-12-22 08:45:26 +0900

    update junit version
    - make test directory
    - make MR3CellMakerTest.java file

---
+++ src/test/java/org/mrcube/utils/MR3CellMakerTest.java
@@ -0,0 +1,40 @@
+package org.mrcube.utils;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class MR3CellMakerTest {
+
+    @BeforeClass
+    void setUp() {
+        fail();
+    }
+
+    @AfterClass
+    void tearDown() {
+        fail();
+    }
+
+    @Test
+    void insertRDFLiteral() {
+        fail();
+    }
+
+    @Test
+    void insertRDFResource() {
+        fail();
+    }
+
+    @Test
+    void insertClass() {
+        fail();
+    }
+
+    @Test
+    void insertProperty() {
+        fail();
+    }
+}
