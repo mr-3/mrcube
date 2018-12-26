b95ee3647b3bcc1820d9ee2bd269be702ee4a6d9
Takeshi Morita <t_morita@keio.jp>
2018-12-26 16:59:06 +0900

    update build.gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module org.mrcube.mrcube {
     requires jgraph;
     requires org.apache.jena.core;
     requires jfontchooser;
+    requires idw;
     exports org.mrcube;
     exports org.mrcube.jgraph;
     exports org.mrcube.models;
