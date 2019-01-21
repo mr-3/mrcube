904c57f79c085fd4a9b46eed3679f36b82455ab2
Takeshi Morita <t_morita@keio.jp>
2019-01-21 12:33:14 +0900

    check desktop supported action

--- src/main/java/org/mrcube/MR3.java
+++ src/main/java/org/mrcube/MR3.java
@@ -121,7 +121,9 @@ public class MR3 extends JFrame implements ChangeListener {
         var quitAction = new QuitAction(this);
         if (Desktop.isDesktopSupported()) {
             var desktop = Desktop.getDesktop();
-            desktop.setQuitHandler((e, response) -> quitAction.quitMR3());
+            if (desktop.isSupported(Desktop.Action.APP_QUIT_HANDLER)) {
+                desktop.setQuitHandler((e, response) -> quitAction.quitMR3());
+            }
         }
         setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
