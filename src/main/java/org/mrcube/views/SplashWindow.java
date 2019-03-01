171e89479532846eebd21962312240953ff98dc2
Takeshi Morita <t_morita@keio.jp>
2019-03-01 09:25:55 +0900

    fix version

--- src/main/java/org/mrcube/views/SplashWindow.java
+++ src/main/java/org/mrcube/views/SplashWindow.java
@@ -39,8 +39,7 @@ public class SplashWindow extends JWindow {
 
 	private static final int FONT_SIZE = 14;
 	private static final String TOOL_NAME = "Project Name: MR<sup>3</sup> <br>";
-	private static final int MINOR_VERSION = 1;
-	private static final String VERSION = "   Version: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM_")) + MINOR_VERSION + "<br>";
+	private static final String VERSION = "   Version: 2019.03_1 <br>";
 	private static final String MR3_URL = " Project Website:   http://mrcube.org/<br>";
 	private static final String COPY_RIGHT = "   Copyright (C) 2003-2019 Yamaguchi Laboratory, Keio University.<br>";
 	private static final String LICENSE = "License: GPL<br>";
