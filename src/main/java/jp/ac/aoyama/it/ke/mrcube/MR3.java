27a4f473f012b1101734d08edde26d3784868b24
Takeshi Morita <t_morita@keio.jp>
2026-04-11 12:04:08 +0900

    Fixed the issue where High DPI was not supported.

--- src/main/java/jp/ac/aoyama/it/ke/mrcube/MR3.java
+++ src/main/java/jp/ac/aoyama/it/ke/mrcube/MR3.java
@@ -641,7 +641,6 @@ public class MR3 extends JFrame implements ChangeListener {
             System.setProperty("apple.laf.useScreenMenuBar", "true");
             FlatMacLightLaf.setup();
         } else {
-            System.setProperty("sun.java2d.uiScale", "1.0");
             FlatLightLaf.setup();
         }
         JenaSystem.init();
